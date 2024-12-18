70774be397e2ace64ec6f55cb896697b5fb349a5
OstappMelnykk <79ostap@ukr.net>
2024-12-19 00:09:38 +0200

    Add all required logic for the access modifiers projects

--- CSharp/Access.Modifiers.Helper/Program.cs
+++ CSharp/Access.Modifiers.Helper/Program.cs
@@ -4,6 +4,28 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        A a = new A(); // public 
+        // B b = new B(); // internal 
+        C c = new C(); // public 
+        
+        
+        // Console.WriteLine(c.a); // private 
+        // Console.WriteLine(c.b); // protected
+        // Console.WriteLine(c.c); // internal 
+        Console.WriteLine(c.d); // public 
+        
+        // Console.WriteLine(c.e); // protected internal
+        // Console.WriteLine(c.f); // private protected
+        
     }
+}
+public class D : A
+{
+    // public new void Print_a() => Console.WriteLine(this.a); // private 
+    public new void Print_b() => Console.WriteLine(this.b); // protected
+    // public new void Print_c() => Console.WriteLine(this.c); // internal 
+    public new void Print_d() => Console.WriteLine(this.d); // public 
+    
+    public new void Print_e() => Console.WriteLine(this.e);  // protected internal
+    // public new void Print_f() => Console.WriteLine(this.f);  // private protected
 }
--- CSharp/Access.Modifiers/Program.cs
+++ CSharp/Access.Modifiers/Program.cs
@@ -3,7 +3,62 @@
 class Program
 {
     static void Main(string[] args)
-    {
-        Console.WriteLine("Hello, World!");
+    { 
+        A a = new A();
+        B b = new B();
+        C c = new C();
+
+        // Console.WriteLine(c.a); // private 
+        // Console.WriteLine(c.b); // protected
+        Console.WriteLine(c.c); // internal 
+        Console.WriteLine(c.d); // public 
+        
+        Console.WriteLine(c.e); // protected internal
+        // Console.WriteLine(c.f); // private protected
+        
     }
+}
+
+public class A
+{
+    private int a;
+    protected int b;
+    internal int c;
+    public int d;
+
+    // З будь-якого місця в коді в межах того ж проекту
+    // З похідних класів, навіть якщо вони в іншому проекті
+    protected internal int e;
+    
+    // Тільки в межах поточного класу.
+    // В межах похідних класів, але лише якщо вони в тому ж проекті
+    private protected int f;
+    
+    //  protected internal — дозволяє доступ у всьому проекті +
+    //                       похідним класам в інших проектах.
+    //
+    //  private protected — доступ тільки в межах проекту,
+    //                      і лише похідним класам.
+
+    public void Print_a() => Console.WriteLine(this.a); // private 
+    public void Print_b() => Console.WriteLine(this.b); // protected
+    public void Print_c() => Console.WriteLine(this.c); // internal 
+    public void Print_d() => Console.WriteLine(this.d); // public 
+    
+    public void Print_e() => Console.WriteLine(this.e); // protected internal
+    public void Print_f() => Console.WriteLine(this.f); // private protected
+
+}
+
+internal class B { }
+
+public class C : A
+{ 
+    // public new void Print_a() => Console.WriteLine(this.a); // private 
+    public new void Print_b() => Console.WriteLine(this.b); // protected
+    public new void Print_c() => Console.WriteLine(this.c); // internal 
+    public new void Print_d() => Console.WriteLine(this.d); // public 
+    
+    public new void Print_e() => Console.WriteLine(this.e);  // protected internal
+    public new void Print_f() => Console.WriteLine(this.f);  // private protected
 }
