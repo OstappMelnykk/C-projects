4093d2b88ee1e852518e32e7bf21a618227b712c
OstappMelnykk <79ostap@ukr.net>
2024-12-18 23:52:34 +0200

    Add new projects for explanation access modifiers
    -The project Access.Modifiers is the main one to explain this topic
    -The project Access.Modifiers.Helper is for explanation what's the internal access modifier

---
+++ CSharp/Access.Modifiers.Helper/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace Access.Modifiers.Helper;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
---
+++ CSharp/Access.Modifiers/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace Access.Modifiers;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
