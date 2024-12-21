ed5f27814bcf3980ab40521f5cc64f4dc7bcadf9
OstappMelnykk <79ostap@ukr.net>
2024-12-21 22:10:59 +0200

    Add new project to view what methods has System.Object type

---
+++ CSharp/SystemObjectMethods/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Reflection;
+
+namespace SystemObjectMethods;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Type type = typeof(System.Object);
+
+        // Отримуємо всі методи типу
+        
+        //MethodInfo[] methods = type.GetMethods();
+
+        // Виводимо всі методи
+        foreach (var method in type.GetMethods())
+        {
+            Console.WriteLine($"{method.Name} - {method.ReturnType.Name}");
+        }
+        
+        /*
+
+            GetType - Type
+            ToString - String
+            Equals - Boolean
+            Equals - Boolean
+            ReferenceEquals - Boolean
+            GetHashCode - Int32
+
+        */
+        
+
+    }
+}
